b850bcf2301126ee78b3495f56eba1cc29ecf106
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-05-30 15:29:33 +0200

    more tests

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,14 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import Home from "@/app/page";
 
-it("Should have Docs text", () => {
-  render(<Home />); //ARRANGE
-
-  const myEl = screen.getByText("Docs"); //ACT
-
-  expect(myEl).toBeInTheDocument(); //ASSERT
-});
-
 //Triple A (AAA) Pattern - Arrange, Act, Assert
 // First we arrange, then we take action and then we assert and make sure
 //that everything is as expected.
+
+describe("Home", () => {
+  it("Should have Docs text", () => {
+    render(<Home />); //ARRANGE
+
+    const myEl = screen.getByText("Docs"); //ACT
+
+    expect(myEl).toBeInTheDocument(); //ASSERT
+  });
+
+  it('Should contain the text "information"', () => {
+    render(<Home />);
+    const myEl = screen.getByText(/information/i);
+    expect(myEl).toBeInTheDocument();
+  });
+
+  it("Should have a heading", () => {
+    render(<Home />);
+    const myEl = screen.getByRole("heading", {
+      name: "Learn",
+    });
+    expect(myEl).toBeInTheDocument();
+  });
+});
